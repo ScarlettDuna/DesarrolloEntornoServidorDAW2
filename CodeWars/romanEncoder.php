eb13fd4aa342117a3d1bd96f6fd21dafb8f6c486
Arantxa Reinoso <154524564+ScarlettDuna@users.noreply.github.com>
2025-11-27 11:58:07 +0100

    reto codewars

--- CodeWars/romanEncoder.php
+++ CodeWars/romanEncoder.php
@@ -12,50 +12,53 @@
 "I"       ->    1
 */
 function solution($number){
+    $numbers = str_split(strrev((String)$number));
+    if (count($numbers) > 4) {
+        echo "Invalid input";
+        return;
+    }
+    $romano = ""; // numbers = [1,6,6,6]
+    $symbols = [
+        ['I', 'V', 'X'],  // unidades
+        ['X', 'L', 'C'],  // decenas
+        ['C', 'D', 'M'],  // centenas
+        ['M']             // miles
+    ];
+    for ($i = count($numbers) - 1; $i >= 0; $i--) {
+        $digit = $numbers[$i];
+
+        if ($i === 3) {
+            // Miles → solo 'M'
+            $romano .= str_repeat('M', $digit);
+            continue;
+        }
+
+        [$one, $five, $ten] = $symbols[$i];
+
+        switch ($digit) {
+            case 1:
+            case 2:
+            case 3:
+                $romano = $romano . str_repeat($one, $digit);
+                break;
+            case 4:
+                $romano = $romano . $one . $five;
+                break;
+            case 5:
+                $romano = $romano . $five;
+                break;
+            case 6:
+            case 7:
+            case 8:
+                $romano = $romano . $five . str_repeat($one, $digit - 5);
+                break;
+            case 9:
+                $romano = $romano . $one. $ten ;
+                break;
+        }
+    }
     
-    return "";
+    return $romano;
 }
-$number = 1666;
-$numbers = str_split(strrev((String)$number));
-print_r($numbers);
-$romano = ""; // numbers = [1,6,6,6]
-$symbols = [
-    ['I', 'V', 'X'],  // unidades
-    ['X', 'L', 'C'],  // decenas
-    ['C', 'D', 'M'],  // centenas
-    ['M']             // miles
-];
-for ($i = count($numbers) - 1; $i >= 0; $i--) {
-    $digit = $numbers[$i];
-
-    if ($i === 3) {
-        // Miles → solo 'M'
-        $romano .= str_repeat('M', $digit);
-        continue;
-    }
-
-    [$one, $five, $ten] = $symbols[$i];
-
-    switch ($digit) {
-        case 1:
-        case 2:
-        case 3:
-            $romano = str_repeat($one, $digit) . $romano;
-            break;
-        case 4:
-            $romano = $one . $five . $romano;
-            break;
-        case 5:
-            $romano = $five . $romano;
-            break;
-        case 6:
-        case 7:
-        case 8:
-            $romano = $five . str_repeat($one, $digit - 5) . $romano;
-            break;
-        case 9:
-            $romano = $one . $ten . $romano;
-            break;
-    }
-}
-echo $romano;
+$number = 78;
+echo solution($number);
