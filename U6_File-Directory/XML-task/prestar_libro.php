b60340447b9151361c3c3062030bc72ee152ec95
Arantxa Reinoso <154524564+ScarlettDuna@users.noreply.github.com>
2025-12-10 13:26:26 +0100

    termiar funcionalidad prestar_libro

--- U6_File-Directory/XML-task/prestar_libro.php
+++ U6_File-Directory/XML-task/prestar_libro.php
@@ -1,6 +1,13 @@
 <?php
+include 'Fase3.php';
 $xmlDom = new DOMDocument();
-$xmlDom->load('libro.xml');
+$xmlDom->load('biblioteca.xml');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $fechaDevolucionTimestamp = time() + 14 * 24 * 3600;
+    $fechaDevolucion = date('d/m/Y', $fechaDevolucionTimestamp);
+    prestarLibro($xmlDom, $_POST['librosLibres'], $fechaDevolucion);
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -13,14 +20,22 @@ $xmlDom->load('libro.xml');
 </head>
 <body>
     <h1>Préstamo de libros</h1>
-    <form action="">
+    <form method="post">
         <select name="librosLibres" id="librosLibres">
             <?php
             foreach ($xmlDom->getElementsByTagName('libro') as $libro) {
-                $libro =
+                $titulo = $libro->getElementsByTagName('titulo')->item(0)->nodeValue;
+                $autor = $libro->getElementsByTagName('autor')->item(0)->nodeValue;
+                $isbn = $libro->getElementsByTagName('isbn')->item(0)->nodeValue;
+                $estado = $libro->getElementsByTagName('estado')->item(0)->nodeValue;
+                if ($estado == 'libre') {
+                    echo '<option value="' . $isbn . '">' . $titulo . ' - ' . $autor . '</option>';
+                }
             }
             ?>
         </select>
+        <button type="submit">Prestar libro</button>
     </form>
+    <a href="Fase4.php">Volver a servicios biblioteca</a>
 </body>
 </html>
