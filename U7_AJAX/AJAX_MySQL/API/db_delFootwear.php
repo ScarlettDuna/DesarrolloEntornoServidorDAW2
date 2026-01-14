0ec152bb9a43b8401be9af0281c573c933ccb78e
Arantxa Reinoso <154524564+ScarlettDuna@users.noreply.github.com>
2026-01-14 11:48:49 +0100

    clase miércoles 14 enero

--- U7_AJAX/AJAX_MySQL/API/db_delFootwear.php
+++ U7_AJAX/AJAX_MySQL/API/db_delFootwear.php
@@ -1,1 +1,32 @@
 <?php
+$id = $_GET["id"];
+// Comprobamos que 'id' es un int (valor posible)
+if (ctype_digit($id)) {
+    try {
+        $pdo = new PDO(
+            "mysql:host=localhost;dbname=clothes;charset=utf8",
+            "root",
+            ""
+        );
+        // Borrar
+        $stmtcheck = $pdo->prepare("SELECT * FROM footwear WHERE id = :id");
+        $stmtcheck->execute([":id", $id]);
+        $check = $stmtcheck->fetch();
+        // Si el ID existe en la base de datos, borramos
+        if ($check) {
+            $stmtdelete = $pdo->prepare("DELETE FROM footwear WHERE id=?");
+            $stmtdelete->execute([$id]);
+            // Devolver listado actualizado
+            $query = $pdo->query("SELECT * FROM footwear");
+            $footwear = $query->fetchAll(PDO::FETCH_ASSOC);
+            $json = json_encode($footwear);
+            echo $json;
+        } else {
+            echo '{"message":"Id no encontrado en la base de datos"}';
+        }
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
+} else {
+    echo '{"message":"Id incorrecto"}';
+}
---
+++ U7_AJAX/AJAX_MySQL/app.js
@@ -0,0 +1,24 @@
+let workplace = document.getElementById('DBworkplace');
+let result = document.getElementById('DBresult')
+function menu(){
+    const value = document.getElementById('actionDB').value;
+    switch (value) {
+        case "ModFootwear":
+
+            break;
+        case "InPants":
+
+            break;
+        case "Load":
+
+            break;
+        case "RegOutfit":
+
+            break;
+        case "DelFootwear":
+
+            break;
+        default:
+
+    }
+}
