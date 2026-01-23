c458f580716177a68bb789949d0e1b6eddb1c2d4
Arantxa Reinoso <154524564+ScarlettDuna@users.noreply.github.com>
2026-01-23 14:13:01 +0100

    Clase viernes 23 enerdo - Symphony - Doctrine

---
+++ U5_Doctrine/doctrine_project/src/Entity/People.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PeopleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PeopleRepository::class)]
+class People
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
